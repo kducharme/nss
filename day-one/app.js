dc549579dd4c2f4bf3dcee6d0f5e0dde57080ed0
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-02 15:05:30 -0500

    finished exercise

--- day-one/app.js
+++ day-one/app.js
@@ -1,10 +1,18 @@
 addMission();
+addZip();
 
+// Adds representatives mission statement to the page
 function addMission() {
     let rep = document.querySelector('#rep'),
-        newNode = document.createElement('section'),
+        newSection = document.createElement('section'),
         mission = "My mission is to do good things";
     
-    newNode.textContent = mission;
-    rep.appendChild(newNode);
+    newSection.textContent = mission;
+    rep.appendChild(newSection);
+}
+
+// Creates new attribute for representative to include zip code
+function addZip() {
+    let rep = document.querySelector('#rep');
+    rep.setAttribute('congressional-district', '37209');
 }
