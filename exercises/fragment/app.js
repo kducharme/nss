6969413fa7edba12d2cc8a352ac1f8d0bc74a217
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-16 14:07:43 -0500

    refactored js

--- exercises/fragment/app.js
+++ exercises/fragment/app.js
@@ -1,12 +1,3 @@
-
-// // Put an article DOM element in your index.html with the id attribute value of messages.
-
-// // In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
-
-// // Create five (5) section elements, each with a class of message, and with textContent of your choosing.
-
-// // Using appendChild(), attach each message as a child to the messages element.
-
 const createElements = () => {
 
     const fragment = document.createDocumentFragment(),
@@ -19,7 +10,6 @@ const createElements = () => {
         section.classList.add('message');
         fragment.appendChild(section)
     })
-
     messages.appendChild(fragment)
 }
 
