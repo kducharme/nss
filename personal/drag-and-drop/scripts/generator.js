322c6a62c977761dbe1a1a7fef7eb70fe052d4b3
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-18 20:30:24 -0500

    reorganized js files

---
+++ personal/drag-and-drop/scripts/generator.js
@@ -0,0 +1,7 @@
+function* idMaker() {
+    let index = 3;
+    while (index < index + 1)
+        yield index++;
+}
+
+const gen = idMaker();git ad
--- personal/drag-and-drop/scripts/tasks.js
+++ personal/drag-and-drop/scripts/tasks.js
@@ -1,7 +1,12 @@
 // Opens modal to create new task
 const taskModal = () => {
-    let modal = document.querySelector('#modal'),
-        close = document.querySelector('#closeModal').addEventListener('click', closeModal);
+    let modal = document.querySelector('#modal');
+    close = document.querySelector('#closeModal').addEventListener('click', closeModal);
+    const priorities = document.querySelectorAll('.priority');
+
+    priorities.forEach(priority => {
+        priority.addEventListener('click', getPriority);
+    })
 
     modal.classList.toggle('hide')
 }
@@ -16,24 +21,42 @@ const closeModal = () => {
 
 // Clears fields after the create task button is clicked
 const clearFields = () => {
-    let name = document.querySelector('#taskName'),
-        description = document.querySelector('#taskDescription');
+    const name = document.querySelector('#taskName');
+    const details = document.querySelector('#taskDetails');
 
     name.value = "";
-    description.value = "";
+    details.value = "";
+}
+
+const getPriority = (e) => {
+    const priorities = document.querySelectorAll('.priority');
+    const selected = e.target;
+
+    priorities.forEach(priority => {
+        if (selected === priority) {
+            priority.classList.add('selected')
+            priority.classList.remove('not-selected')
+        }
+        else {
+            priority.classList.remove('selected')
+            priority.classList.add('not-selected')
+        }
+    })
+    return selected.id;
 }
 
 // Creates an object for the task and triggers posting to DOM
 const createTask = () => {
     const name = document.querySelector('#taskName').value;
-    const description = document.querySelector('#taskDescription').value;
-    const priority = document.querySelector('#taskPriority');
+    const details = document.querySelector('#taskDetails').value;
+    const priority = '';
+    const progress = '';
 
     const task = Object.create(null, {
         title: { writable: true, value: name },
         details: { writable: true, value: details },
         priority: { writable: true, value: priority },
-        progress: { writable: true, value: description }
+        progress: { writable: true, value: progress }
     })
 
     clearFields();
@@ -44,24 +67,17 @@ const createTask = () => {
 
 const createNewTask = document.querySelector('#createTask').addEventListener('click', createTask)
 
-function* idMaker() {
-    let index = 3;
-    while (index < index + 1)
-        yield index++;
-}
-let gen = idMaker();
-
 // Posts tasks to the DOM
 const postTask = (task) => {
-    let name = task.taskName,
-        description = task.taskDescript,
+    let name = task.title,
+        details = task.details,
         structure = document.createElement('span'),
         taskHead = document.createElement('h2'),
         taskBody = document.createElement('p'),
         backlog = document.querySelector('#tasks-backlog');
 
     taskHead.textContent = name;
-    taskBody.textContent = description;
+    taskBody.textContent = details;
 
     structure.setAttribute('draggable', 'true');
     structure.setAttribute('ondragstart', 'drag(event)');
@@ -88,6 +104,10 @@ const taskCount = () => {
 
     printTotal.classList.add('total-tasks');
     printTotal.textContent = `(${count} tasks)`
+}
+
+const printTaskCount = () => {
+
 }
 
 taskCount();
