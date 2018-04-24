a0d9408664556683eef72b1588a5f3c19c197bc9
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-24 08:26:09 -0500

    refactored code

--- exercises/chainMethods/challenge.js
+++ exercises/chainMethods/challenge.js
@@ -5,61 +5,57 @@ const printArea = document.querySelector('#printArea')
 const salesData = [];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+// Total profit for 2017
 const totalProfit = () => {
     const el = document.createElement('p')
-    let totalSales = 0;
-    data.forEach(sale => {
-        for (let key in sale) {
-            if (key === 'gross_profit') {
-                totalSales += sale[key]
-                salesData.push(sale[key])
-            }
-        }
-    })
+    const totalSales = data.reduce((a, b) => {
+        return a + b.gross_profit
+    }, 0)
     el.textContent = `Total profit: ${totalSales}`
     printArea.appendChild(el)
 }
 
+// In which month did they sell the most cars?
 const bestSalesMonth = () => {
     const el = document.createElement('p')
-    const highestSale = salesData.sort((a, b) => a < b ? 1 : -1).splice(0, 1)[0]
-    data.forEach(sale => {
-        for (let key in sale) {
-            if (sale[key] === highestSale) {
-                let date = (sale.purchase_date.split('-')[1])
-                let salesPerson = sale.sales_agent.first_name + ' ' + sale.sales_agent.last_name;
-                if (date.startsWith(0)) {
-                    date = date.split('')[1];
-                }
-                date = months[date - 1];
-                el.textContent = `Best sales month: ${date} ($${highestSale}) by ${salesPerson}`
-                printArea.appendChild(el)
-            }
-        }
-    })
-}
+    let bestMonth = data.sort((a, b) => a.gross_profit < b.gross_profit ? 1 : -1).splice(0, 1)[0].purchase_date.split('-')[1]
 
-// 3. Which salesperson sold the most cars?
-const mostTransactions = () => {
-    const el = document.createElement('p')
-
-    const salesPerPerson = data.map(sale => {
-        const saleInfo = {};
-        saleInfo.profit = sale.gross_profit;
-        saleInfo.person = sale.sales_agent.first_name + ' ' + sale.sales_agent.last_name;
-        return saleInfo
-    })
-
-    const result = [...salesPerPerson.reduce( (total, next) => {
-        if (!total.has(next.person)) total.set(next.person, Object.assign({ count: 0 }, next));
-        total.get(next.person).count++;
-        return total;
-    }, new Map).values()];
-    
-    const mostSales = result.sort((a, b) => a.count < b.count ? 1 : -1).splice(0, 1)[0]
-    el.textContent = `The sales person with the most sales is ${mostSales.person} with ${mostSales.count} total sales.`
+    if (bestMonth.startsWith(0)) {
+        bestMonth = bestMonth.split('')[1];
+    }
+    bestMonth = months[bestMonth - 1];
+    el.textContent = `The best sales month was: ${bestMonth}`;
     printArea.appendChild(el)
 }
+
+// Which salesperson sold the most cars?
+const bestSalesPerson =() => {
+    const el = document.createElement('p')
+
+    let allMonths = data.sort((a, b) => a.gross_profit < b.gross_profit ? 1 : -1)
+
+    const result = [...allMonths.reduce( (total, next) => {
+        if (!total.has(next.sales_agent.last_name)) total.set(next.first_name, Object.assign({ count: 0 }, next));
+        total.get(next.person).count++;
+
+        return total;
+    }, new Map).values()];
+
+    console.log(result)
+
+    el.textContent = result;
+    printArea.appendChild(el)
+
+}
+
+
+
+
+
+
+
+
+
 
 const mostTotalSales = () => {
     const el = document.createElement('p')
@@ -71,18 +67,7 @@ const mostTotalSales = () => {
         return saleInfo
     })
 
-    const result = [...salesPerPerson.reduce( (total, next) => {
-        if (!total.has(next.person)) total.set(next.person, Object.assign({ totalSales: 0 }, next));
-        total.get(next.person).totalSales;
 
-        return totalSales;
-    }, new Map).values()];
-
-    console.log(result)
-    
-    const mostSales = result.sort((a, b) => a.count < b.count ? 1 : -1).splice(0, 1)[0]
-    el.textContent = `The sales person with the most sales is ${mostSales.person} with ${mostSales.count} total sales.`
-    printArea.appendChild(el)
 }
 
 
@@ -93,5 +78,5 @@ const mostTotalSales = () => {
 
 totalProfit()
 bestSalesMonth()
-mostTransactions()
 mostTotalSales()
+bestSalesPerson()
