c9bfb2b7b44b506249aad093ff778faeaf31196c
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-25 09:18:42 -0500

    done

--- exercises/chainMethods/app.js
+++ exercises/chainMethods/app.js
@@ -3,4 +3,4 @@
 // 4. Then output (either in the DOM or the console) the sum of all the resulting numbers.
 const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-let newOutput = integers.sort((a,b) => a < b ? 1 : -1).filter(int => int > 19).map(int => (int * 1.5) -1).reduce((total, next) => total + next)
+let newOutput = integers.sort((a,b) => a < b ? 1 : -1).filter(int => int <= 19).map(int => (int * 1.5) -1).reduce((total, next) => total + next)
--- exercises/chainMethods/challenge.js
+++ exercises/chainMethods/challenge.js
@@ -1,4 +1,3 @@
-// 4. Which salesperson made the most profit?
 // 5. Which model was the most popular?
 // 6. Which bank provided the most loans to our customers?
 const printArea = document.querySelector('#printArea')
@@ -8,7 +7,7 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 // Total profit for 2017
 const totalProfit = () => {
     const el = document.createElement('p')
-    const totalSales = data.reduce((a, b) => {
+    const totalSales = data.map((a, b) => {
         return a + b.gross_profit
     }, 0)
     el.textContent = `Total profit: ${totalSales}`
@@ -18,13 +17,26 @@ const totalProfit = () => {
 // In which month did they sell the most cars?
 const bestSalesMonth = () => {
     const el = document.createElement('p')
-    let bestMonth = data.sort((a, b) => a.gross_profit < b.gross_profit ? 1 : -1).splice(0, 1)[0].purchase_date.split('-')[1]
+    const salesMonths =[];
+    const allMonths = data.map(sale => {
+        return sale.purchase_date
+    }, 0)
 
-    if (bestMonth.startsWith(0)) {
-        bestMonth = bestMonth.split('')[1];
-    }
-    bestMonth = months[bestMonth - 1];
-    el.textContent = `The best sales month was: ${bestMonth}`;
+    allMonths.forEach(month => {
+        month = new Date(month).getMonth()
+        month = months[month - 1]
+        salesMonths.push(month)
+    });
+
+    const bestSales = [...salesMonths.reduce((total, next) => {
+        if (!total.has(next)) total.set(next, Object.assign({ count: 0 }, next));
+        total.get(next).count++;
+        return total;
+    }, new Map).values()].sort((a, b) => a.count < b.count ? 1 : -1)[0];
+
+    console.log(bestSales)
+
+    // el.textContent = `The best sales month was: ${bestMonth}`;
     printArea.appendChild(el)
 }
 
@@ -37,14 +49,26 @@ const bestSalesPerson = () => {
         if (!total.has(next.sales_agent.last_name)) total.set(next.sales_agent.last_name, Object.assign({ count: 0 }, next));
         total.get(next.sales_agent.last_name).count++;
         return total;
-    }, new Map).values()].sort((a,b) => a.count < b.count ? 1 : -1)[0].sales_agent;
+    }, new Map).values()].sort((a, b) => a.count < b.count ? 1 : -1)[0].sales_agent;
 
     const name = `${result.first_name} ${result.last_name} sold the most cars`;
 
+    console.log
     el.textContent = name;
     printArea.appendChild(el)
 }
 
+// Which salesperson made the most profit?
+const mostProfit = () => {
+    const el = document.createElement('p')
+    let allMonths = data.sort((a, b) => a.gross_profit < b.gross_profit ? 1 : -1)
+
+    const totalSales = data.reduce((a, b) => {
+        return a + b.gross_profit
+    }, 0)
+
+
+}
 
 
 
