21431cfaf6a8801361818780741cd9c2eeb90819
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-08 11:46:10 -0500

    completed

--- exercises/testApp/src/scripts/main.js
+++ exercises/testApp/src/scripts/main.js
@@ -1,7 +1,22 @@
 const $ = require('jquery');
 
 $.ajax('http://localhost:8088/food')
-    .then(response => {
-        console.log(response)
+    .then(data => {
+        console.log(data);
+        parseData(data)
     })
 
+const parseData = (data) => {
+    const allFood = Object.keys(data)
+    allFood.forEach(item => {
+        const { id, name, type, ethnicity, sideDishes } = data[item]
+        const $structure = document.createElement('span');
+        $structure.append(id, name, type, ethnicity, sideDishes)
+        printData($structure)
+    })
+}
+
+const printData = ($structure) => {
+    const $printArea = $('#printArea');
+    $printArea.append($structure)
+}
